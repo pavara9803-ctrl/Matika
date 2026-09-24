f3333efb7178d116895811c30193cf7c888b012e
pavara9803-ctrl <pavaracitta9803@gmail.com>
2026-09-24 20:21:16 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js - Service Worker for Offline Support
 // දුක මාතිකා යෙදුම සඳහා නොබැඳි (Offline) සහාය
 
-const CACHE_NAME = 'duka-matika-v3.3.3'; // ✅ අලුත් version
+const CACHE_NAME = 'duka-matika-v3.3.4'; // ✅ අලුත් version
 const OFFLINE_URL = './index.html';
 
 // ============================================================
