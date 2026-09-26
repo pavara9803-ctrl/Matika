4aa3d9820af068c95cdbcc51982cc8c060a0a702
pavara9803-ctrl <pavaracitta9803@gmail.com>
2026-09-26 18:13:26 +0530

    Update service worker to version 9 and improve caching

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // Version: 8.0.0
 // ============================================================
 
-const CACHE_NAME = 'abhidhamma-matika-v8';
+const CACHE_NAME = 'abhidhamma-matika-v9';
 const OFFLINE_URL = './index.html';
 
 // ============================================================
